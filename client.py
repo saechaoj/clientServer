7d8096b84a689bf18b408636a7d546370faae6a3
Jarrod Saechao <46543713+saechaoj@users.noreply.github.com>
2022-12-03 19:10:09 -0800

    Add files via upload

--- client.py
+++ client.py
@@ -1,21 +1,34 @@
+#Tutorial https://medium.com/@jkishan421/chat-room-application-in-python-part-i-9193d768dc64
+
+
 import socket
 
 host = 'localhost'
 port = 3000
 
+#Create Socket and connect connect to local host 3000
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.connect((host, port))
 
 
-#Get message from server
-msg = sock.recv(1024).decode()
-
 #Continuous chat
-while msg:
-    print("Message : ", msg)
-    msg = sock.recv(1024).decode()
+while 1:
 
-    #Check for quit message
-    if msg == '/q':
+    #Send Message in byte array or quit
+    sendMsg = input("Send Message:")
+    if sendMsg == "/q":
         sock.close()
+        break
+    else:
+        sock.send(sendMsg.encode())
+
+
+    #Recieves message from Server
+    print("Waiting for message...")
+    msg = sock.recv(1024).decode()
+    print("Recieved: ", msg)
+
+
+    
+print("Program End")
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+#Tutorial https://medium.com/@jkishan421/chat-room-application-in-python-part-i-9193d768dc64
+
+
 import socket
 
 
@@ -5,26 +8,38 @@ import socket
 host = 'localhost'
 port = 3000
 
-# creating a socket with ipv4 protocol and binding socket
+# creating a socket with ipv4 protocol and binding socket for Sending
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port)) 
 
 
-
-
 # server listens to accept the connecion. Socket listens for 1 client
 sock.listen(1)
-print("Server Listening on Port: ", port)
+print("Server Running...")
 
+
+
+#creates new socket instance for this connection and display attributes of new client
 connection, address = sock.accept()
-print("Client Connected ", connection)
-print("Client Address", address)
 
 
-# create message on server-side
-message = "This is the server, Hellooooooo"
+# print("Client Connected ", connection)
+print("Client Connected", address)
+
+#Send message or recieve
+while 1:
+    
+    print("Waiting for message...")
+    msg = connection.recv(1024).decode()
+    print("Recieved:",msg)
+
+    #Reply in byte array or quits
+    message = input("Send Message:")
+    if message == "/q":
+        sock.close()
+        break
+    else:
+        connection.send(message.encode())
 
 
-# send message to the client in form of bytes array.
-connection.send(message.encode())
-connection.close()
+print("Program End")
